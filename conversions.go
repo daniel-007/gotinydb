cdfc1a8983035fdc6b0df078835b6456ba978efc
Alexandre Stein <alexandre_stein@interlab-net.com>
2018-07-10 16:02:13 +0200

    NOT working

--- conversions.go
+++ conversions.go
@@ -2,7 +2,9 @@ package gotinydb
 
 import (
 	"encoding/binary"
+	"fmt"
 	"math"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -24,20 +26,20 @@ func stringToBytes(input interface{}) ([]byte, error) {
 // to bytes slice. If an error is returned it's has the form of ErrWrongType
 func intToBytes(input interface{}) ([]byte, error) {
 	typedValue := uint64(0)
-	switch input.(type) {
+	switch this := input.(type) {
 	case int, int8, int16, int32, int64:
 		typedValue = convertIntToAbsoluteUint(input)
 
 	case uint:
-		typedValue = uint64(input.(uint))
+		typedValue = uint64(this)
 	case uint8:
-		typedValue = uint64(input.(uint8))
+		typedValue = uint64(this)
 	case uint16:
-		typedValue = uint64(input.(uint16))
+		typedValue = uint64(this)
 	case uint32:
-		typedValue = uint64(input.(uint32))
+		typedValue = uint64(this)
 	case uint64:
-		typedValue = input.(uint64)
+		typedValue = this
 	default:
 		return nil, ErrWrongType
 	}
@@ -50,22 +52,68 @@ func intToBytes(input interface{}) ([]byte, error) {
 func convertIntToAbsoluteUint(input interface{}) (ret uint64) {
 	typedValue := int64(0)
 
-	switch input.(type) {
+	switch this := input.(type) {
 	case int:
-		typedValue = int64(input.(int))
+		typedValue = int64(this)
 	case int8:
-		typedValue = int64(input.(int8))
+		typedValue = int64(this)
 	case int16:
-		typedValue = int64(input.(int16))
+		typedValue = int64(this)
 	case int32:
-		typedValue = int64(input.(int32))
+		typedValue = int64(this)
 	case int64:
-		typedValue = int64(input.(int64))
+		typedValue = int64(this)
 	}
 
 	ret = uint64(typedValue) + (math.MaxUint64 / 2) + 1
 
 	return ret
+}
+
+func floatToBytes(input interface{}) ([]byte, error) {
+	negative := false
+	switch this := input.(type) {
+	case float32:
+		if this < 0 {
+			negative = true
+		}
+	case float64:
+		if this < 0 {
+			negative = true
+		}
+	default:
+		return nil, ErrWrongType
+	}
+
+	floatAsString := fmt.Sprintf("%b", input)
+	fmt.Println("floatAsString", floatAsString)
+
+	parts := strings.Split(floatAsString, ".")
+	integer, integerParseErr := strconv.ParseInt(parts[0], 10, 64)
+	if integerParseErr != nil {
+		return nil, integerParseErr
+	}
+	floating, floatingParseErr := strconv.ParseInt(parts[1], 10, 64)
+	if floatingParseErr != nil {
+		return nil, floatingParseErr
+	}
+
+	fmt.Println("floating", floating)
+	if negative {
+		floating = -floating
+	}
+	fmt.Println("floating", floating)
+
+	integerAsBytes, integerToBytesErr := intToBytes(integer)
+	if integerToBytesErr != nil {
+		return nil, integerToBytesErr
+	}
+	floatingAsBytes, floatingToBytesErr := intToBytes(floating)
+	if floatingToBytesErr != nil {
+		return nil, floatingToBytesErr
+	}
+
+	return append(integerAsBytes, floatingAsBytes...), nil
 }
 
 // timeToBytes converter from a time struct to bytes slice.
--- conversions_test.go
+++ conversions_test.go
@@ -110,6 +110,79 @@ func TestIntOrdering(t *testing.T) {
 	}
 }
 
+func TestFloatConversion(t *testing.T) {
+	v := 31497.545785
+	if _, err := floatToBytes(v); err != nil {
+		t.Error(err)
+		return
+	}
+
+	v = -3223597.00004855
+	if _, err := floatToBytes(v); err != nil {
+		t.Error(err)
+		return
+	}
+
+	v = 0.9
+	if _, err := floatToBytes(v); err != nil {
+		t.Error(err)
+		return
+	}
+
+	v = -0.9
+	if _, err := floatToBytes(v); err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+// func TestFloatOrdering(t *testing.T) {
+// 	neg, _ := floatToBytes(-1.0)
+// 	null, _ := floatToBytes(0.0)
+// 	pos, _ := floatToBytes(1.0)
+
+// 	if !reflect.DeepEqual(neg, []byte{63, 240, 0, 0, 0, 0, 0, 0}) {
+// 		t.Errorf("negative values is not what is expected: \n%v\n%v", neg, []byte{63, 240, 0, 0, 0, 0, 0, 0})
+// 	} else if !reflect.DeepEqual(null, []byte{128, 0, 0, 0, 0, 0, 0, 0}) {
+// 		t.Errorf("null values is not what is expected: \n%v\n%v", null, []byte{128, 0, 0, 0, 0, 0, 0, 0})
+// 	} else if !reflect.DeepEqual(pos, []byte{191, 240, 0, 0, 0, 0, 0, 0}) {
+// 		t.Errorf("positive values is not what is expected: \n%v\n%v", pos, []byte{191, 240, 0, 0, 0, 0, 0, 0})
+// 	}
+
+// 	if bytes.Compare(neg, null) >= 0 {
+// 		t.Error("negative values are not smaller than null", neg, null)
+// 	} else if bytes.Compare(null, pos) >= 0 {
+// 		t.Error("null values are not smaller than positive", null, pos)
+// 	} else if bytes.Compare(neg, pos) >= 0 {
+// 		t.Error("negative values are not smaller than positive", neg, pos)
+// 	}
+
+// 	neg, _ = floatToBytes(math.SmallestNonzeroFloat64)
+// 	null, _ = floatToBytes(0.0)
+// 	pos, _ = floatToBytes(math.MaxFloat64)
+
+// 	if !reflect.DeepEqual(neg, []byte{128, 0, 0, 0, 0, 0, 0, 1}) {
+// 		t.Errorf("negative values is not what is expected: \n%v\n%v", neg, []byte{128, 0, 0, 0, 0, 0, 0, 1})
+// 	} else if !reflect.DeepEqual(null, []byte{128, 0, 0, 0, 0, 0, 0, 0}) {
+// 		t.Errorf("null values is not what is expected: \n%v\n%v", null, []byte{128, 0, 0, 0, 0, 0, 0, 0})
+// 	} else if !reflect.DeepEqual(pos, []byte{255, 239, 255, 255, 255, 255, 255, 255}) {
+// 		t.Errorf("positive values is not what is expected: \n%v\n%v", pos, []byte{255, 239, 255, 255, 255, 255, 255, 255})
+// 	}
+
+// 	if bytes.Compare(neg, null) >= 0 {
+// 		t.Error("negative values are not smaller than null", neg, null)
+// 	} else if bytes.Compare(null, pos) >= 0 {
+// 		t.Error("null values are not smaller than positive", null, pos)
+// 	} else if bytes.Compare(neg, pos) >= 0 {
+// 		t.Error("negative values are not smaller than positive", neg, pos)
+// 	}
+
+// 	if _, err := intToBytes(time.Now()); err == nil {
+// 		t.Error(err)
+// 		return
+// 	}
+// }
+
 func TestTimeConversion(t *testing.T) {
 	if _, err := timeToBytes(time.Now()); err != nil {
 		t.Error(err)
