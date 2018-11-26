d3addbc753e9fdd088e948adaf46f528ee20df6f
Alexandre Stein <alexandre_stein@interlab-net.com>
2018-11-26 18:12:01 +0100

    Add a custom listener to have mulptiple endpoint

--- replication/securelink/connector_test.go
+++ replication/securelink/connector_test.go
@@ -4,6 +4,8 @@ import (
 	"crypto/tls"
 	"fmt"
 	"html"
+	"io"
+	"net"
 	"net/http"
 	"sync"
 	"testing"
@@ -11,6 +13,38 @@ import (
 
 	"github.com/alexandrestein/gotinydb/replication/securelink"
 )
+
+var (
+	tlsListener net.Listener
+	httpServer  *http.Server
+	cl          *securelink.Listener
+)
+
+type (
+	handler struct{}
+)
+
+func (h *handler) Handle(conn net.Conn) error {
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		return err
+	}
+
+	buffer = buffer[:n]
+
+	var n2 int
+	n2, err = conn.Write(buffer)
+	if err != nil {
+		return err
+	}
+
+	if n != n2 {
+		return fmt.Errorf("the read and write length are different: %d %d", n, n2)
+	}
+
+	return nil
+}
 
 func TestConnector(t *testing.T) {
 	ca, _ := securelink.NewCA(
@@ -20,6 +54,48 @@ func TestConnector(t *testing.T) {
 		"ca",
 	)
 
+	t.Run("base", func(t *testing.T) {
+		testBase(t, ca)
+	})
+	t.Run("wildcard", func(t *testing.T) {
+		testWildcardCertAndDifferentHandler(t, ca)
+	})
+}
+
+func runSever(t *testing.T, cert *securelink.Certificate, wg *sync.WaitGroup) {
+	tlsConfig := securelink.GetBaseTLSConfig("node", cert)
+
+	var err error
+	tlsListener, err = tls.Listen("tcp", ":6246", tlsConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	httpHandler := http.NewServeMux()
+	httpHandler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	httpServer = &http.Server{
+		TLSConfig: tlsConfig,
+		Handler:   httpHandler,
+	}
+
+	cl = securelink.NewListener(tlsListener)
+	cl.RegisterService("directTLS", func(serverName string) bool {
+		if serverName == "test.node" {
+			cl.Addr()
+			return true
+		}
+
+		return false
+	}, new(handler))
+
+	wg.Done()
+	httpServer.Serve(cl)
+}
+
+func testBase(t *testing.T, ca *securelink.Certificate) {
 	cert, _ := ca.NewCert(securelink.KeyTypeEc, securelink.KeyLengthEc256,
 		time.Hour,
 		securelink.GetCertTemplate(nil, nil),
@@ -33,23 +109,86 @@ func TestConnector(t *testing.T) {
 
 	wg := sync.WaitGroup{}
 	wg.Add(1)
-	go func() {
-		listener, err := tls.Listen("tcp", ":6246", securelink.GetBaseTLSConfig("node", cert))
-		if err != nil {
-			t.Fatal(err)
-		}
-		wg.Done()
-
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-			defer listener.Close()
-		})
-		http.Serve(listener, nil)
-	}()
+	go runSever(t, cert, &wg)
 	wg.Wait()
 
 	cli := securelink.NewConnector("node", cliCert)
 	_, err := cli.Get("https://127.0.0.1:6246/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cli = securelink.NewConnector("node", cliCert)
+	_, err = cli.Get("https://127.0.0.1:6246/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = httpServer.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func testWildcardCertAndDifferentHandler(t *testing.T, ca *securelink.Certificate) {
+	cert, _ := ca.NewCert(securelink.KeyTypeEc, securelink.KeyLengthEc256,
+		time.Hour,
+		securelink.GetCertTemplate(nil, nil),
+		"*.node",
+	)
+	cliCert, _ := ca.NewCert(securelink.KeyTypeEc, securelink.KeyLengthEc256,
+		time.Hour,
+		securelink.GetCertTemplate(nil, nil),
+		"cli",
+	)
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go runSever(t, cert, &wg)
+	wg.Wait()
+
+	// Access directly to the TLS stream with the hander for the sub domain test.node
+	conn, err := tls.Dial("tcp", "127.0.0.1:6246", securelink.GetBaseTLSConfig("test.node", cliCert))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	writeBuff := []byte("HELLO Wold!!!")
+	var n int
+	n, err = conn.Write(writeBuff)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	buff := make([]byte, n)
+	var n2 int
+	n2, err = conn.Read(buff)
+	if err != nil && err != io.EOF {
+		t.Fatal(err)
+	}
+	err = conn.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if n != n2 {
+		t.Fatalf("the write and read length are different: %d %d", n, n2)
+	}
+
+	if s1, s2 := string(writeBuff), string(buff); s1 != s2 {
+		t.Fatalf("the write and read are different: %s %s", s1, s2)
+	}
+
+	// Deregister the direct TLS handler
+	cl.DeregisterService("directTLS")
+
+	// use the same sub domain to access default HTTP interface
+	cli := securelink.NewConnector("test.node", cliCert)
+	_, err = cli.Get("https://127.0.0.1:6246/")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = httpServer.Close()
 	if err != nil {
 		t.Fatal(err)
 	}
---
+++ replication/securelink/customListener.go
@@ -0,0 +1,104 @@
+package securelink
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net"
+)
+
+type (
+	// Listener defines a struct to manage multiple services behind a sign TLS server.
+	// It bases the service definition on the tls serverName client is asking for.
+	Listener struct {
+		tlsListener net.Listener
+		services    []*serviceRaw
+	}
+
+	// ServiceMatch is a simple function type which based on a string tells if
+	// the match is true or not
+	ServiceMatch func(serverName string) (match bool)
+
+	// Handler defines a interface to have other services after the TLS handshake.
+	// Register a new a new service which implement the Handler interface to mix
+	// HTTP with other protocols.
+	Handler interface {
+		Handle(net.Conn) error
+	}
+
+	serviceRaw struct {
+		name          string
+		handler       Handler
+		matchFunction ServiceMatch
+	}
+)
+
+// NewListener builds a new Listener with the given tls listener
+func NewListener(tlsListener net.Listener) *Listener {
+	cl := new(Listener)
+	cl.tlsListener = tlsListener
+	return cl
+}
+
+// Accept implements the net.Listener interface
+func (l *Listener) Accept() (net.Conn, error) {
+	conn, err := l.tlsListener.Accept()
+	if err != nil {
+		return nil, err
+	}
+
+	tlsConn, ok := conn.(*tls.Conn)
+	if !ok {
+		return nil, fmt.Errorf("the connection is not TLS")
+	}
+
+	err = tlsConn.Handshake()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, service := range l.services {
+		if service.matchFunction(tlsConn.ConnectionState().ServerName) {
+			err := service.handler.Handle(tlsConn)
+			if err != nil {
+				return nil, err
+			}
+
+			return tlsConn, tlsConn.Close()
+		}
+	}
+
+	return conn, err
+}
+
+// Close implements the net.Listener interface
+func (l *Listener) Close() error {
+	return l.tlsListener.Close()
+}
+
+// Addr implements the net.Listener interface
+func (l *Listener) Addr() net.Addr {
+	return l.tlsListener.Addr()
+}
+
+// RegisterService adds a new service with it's associated math function
+func (l *Listener) RegisterService(name string, serviceMatchFunc ServiceMatch, handler Handler) {
+	sr := new(serviceRaw)
+	sr.name = name
+	sr.handler = handler
+	sr.matchFunction = serviceMatchFunc
+
+	l.services = append(l.services, sr)
+
+	return
+}
+
+// DeregisterService removes a service base on the index
+func (l *Listener) DeregisterService(name string) {
+	for i, service := range l.services {
+		if service.name == name {
+			copy(l.services[i:], l.services[i+1:])
+			l.services[len(l.services)-1] = nil // or the zero value of T
+			l.services = l.services[:len(l.services)-1]
+		}
+	}
+}
